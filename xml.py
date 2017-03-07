be50d682f05ec04e3e74aed4a30124afcf8208a1
sayth renshaw <sayth@linux-2sg8.gateway>
2017-03-07 22:38:22 +1100

    whipped

--- xml.py
+++ xml.py
@@ -10,7 +10,8 @@ def parseLog(file):
     handler = open(file).read()
     # parser = etree.XMLParser(remove_comments=True)
     # tree = objectify.parse(file, parser=parser)
-    root = objectify.fromstring(handler)
+    parser = objectify.makeparser(recover=True)
+    root = objectify.fromstring(handler, parser)
     print(root.attrib['venue'])
     # race_attrib = root.race.nomination
     # for k in root.race.attrib:
@@ -21,11 +22,22 @@ def parseLog(file):
     # for race in stuff.getchildren():
     #     race_attrib = race.attrib
     #     print(race_attrib)
+    attrs = [
+        'id', 'number', 'horse', 'barrier', 'weight', 'goodtrack',
+        'thisdistance', 'firstup', 'secondup', 'decimalmargin'
+    ]
     for noms in root.race:
         print("#######################################")
+        print('id', 'number', 'horse', 'barrier', 'weight', 'goodtrack',
+              'thisdistance')
         print(noms.attrib.get('id'))
         for item in noms.nomination:
-            print(item.attrib.get('horse'))
+            try:
+                listy = [item.attrib[x] for x in attrs]
+                listy.insert(0, noms.attrib.get('id'))
+                print(listy)
+            except KeyError:
+                continue
 
 
 if __name__ == "__main__":
